5c04402999ff84e2e0dbd33c6a8416c6dda8687d
bassijtsma <btsijtsma@gmail.com>
2016-02-21 23:40:56 +0100

    Added updating the conversationstate

--- mockchatbot.py
+++ mockchatbot.py
@@ -20,6 +20,25 @@ questions = sampledata.getQuestions()
 responses = sampledata.getResponses()
 conversations = sampledata.getConversations()
 
+# Keeps track of the state of different conversations, so different people
+# can talk to the bot at the same time without the chat intermingling a response
+# messageProtocolEntity.getFrom() will be key.The most recent interaction with
+# the bot will be tracked to figure out if the conversation has timed out and
+# should be reset. Finally, it tracks how far into the conversation they are.
+# conversationstate =
+    # {   m.getFrom() : {
+    #         latestInteraction : timestamp,
+    #         mostrecentquestion: question_nr },
+    #     m.getFrom() : {
+    #         latestInteraction : timestamp,
+    #         mostrecentquestion: question_nr },
+    #     m.getFrom() : {
+    #         latestInteraction : timestamp,
+    #         mostrecentquestion: question_nr },
+    # }
+
+conversationstate = []
+
 print questions
 print responses
 print conversations
@@ -46,6 +65,12 @@ def getConvName(conv_id):
             print 'the conv name is:', conv['conv_name']
             return conv['conv_name']
 
+
+# TODO: decide what method of time handling to use
+# must be able to delta different timestamps
+def updateConversationState(messageSender, question_nr):
+    conversationstate[messageSender][latestInteraction] = 0 #TODO timestamp
+    conversationstate[messageSender][question_nr] = question_nr
 
 
 conv_id = defineConvId()
