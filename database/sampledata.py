4bd4b25b9940e207f9b02dcd095988f837f6288e
bassijtsma <btsijtsma@gmail.com>
2016-02-21 17:44:55 +0100

    preparation chatbot backend with mockdata

--- database/sampledata.py
+++ database/sampledata.py
@@ -1,11 +1,51 @@
-questions = {[
-    { "qid" : 1, "text" : "hoi", "conv_id" : 1, 'question_to' : [{ "rid" : "1" }] },
-    { "qid" : 2, "text" : "dubbel hoi", "conv_id" : 1, 'question_to' : [{ "rid" : "2"  }] },
-    { "qid" : 3, "text" : "drie", "conv_id" : 1, 'question_to' : [{ "rid" : "2" }] },
-    { "qid" : 1, "text" : "een conv2", "conv_id" : 2 }
-]}
+#!/usr/bin/python
 
-responses = {[
-    { "rid" : 1, "text" : "doei1antw", "conv_id" : 1, "response_to" : [ { "qid" : 1 } ] },
-    {  "rid" : 2, "text" : "doei23antw", "conv_id" : 1, "response_to" : [ { "qid" : 1 }, { "qid" : 2 }, { "qid" : 3 } ] }
-}]
+class Sampledata:
+
+    def getQuestions(self):
+        return self.q
+
+    def getResponses(self):
+        return self.r
+
+    def getConversations(self):
+        return self.convs
+
+
+    # oldquestions = [
+    #     { "qid" : 1, "text" : "hoi", "conv_id" : 1, 'question_to' : [{ "rid" : "1" }] },
+    #     { "qid" : 2, "text" : "dubbel hoi", "conv_id" : 1, 'question_to' : [{ "rid" : "2"  }] },
+    #     { "qid" : 3, "text" : "drie", "conv_id" : 1, 'question_to' : [{ "rid" : "2" }] },
+    #     { "qid" : 1, "text" : "een conv2", "conv_id" : 2 }
+    # ]
+
+    # oldresponses = [
+    #     { "rid" : 1, "text" : "doei1antw", "conv_id" : 1, "response_to" : [ { "qid" : 1 } ] },
+    #     {  "rid" : 2, "text" : "doei23antw", "conv_id" : 1, "response_to" : [ { "qid" : 1 }, { "qid" : 2 }, { "qid" : 3 } ] }
+    # ]
+
+
+    q = [
+        { "qnr" : 1, "text" : "hoi", "conv_id" : 1},
+        { "qnr" : 2.1, "text" : "hoi21", "conv_id" : 1},
+        { "qnr" : 2.2, "text" : "hoi22", "conv_id" : 1},
+        { "qnr" : 2.3, "text" : "hoi23", "conv_id" : 1},
+        { "qnr" : 3, "text" : "hoi3", "conv_id" : 1},
+        { "qnr" : 1, "text" : "hoic1", "conv_id" : 2},
+        { "qnr" : 2, "text" : "hoic2", "conv_id" : 2}
+    ]
+
+    r = [
+        { "rnr" : 1, "text" : "doei 1", "conv_id" : 1, "response_to_q" : 1},
+        { "rnr" : 2.1, "text" : "doei 21", "conv_id" : 1, "response_to_q" : 2.1},
+        { "rnr" : 2.2, "text" : "doei 22", "conv_id" : 1, "response_to_q" : 2.2},
+        { "rnr" : 2.3, "text" : "doei 23", "conv_id" : 1, "response_to_q" : 2.3},
+        { "rnr" : 3, "text" : "doei 3", "conv_id" : 1, "response_to_q" : 3},
+        { "rnr" : 1, "text" : "doei c1", "conv_id" : 2, "response_to_q" : 1},
+        { "rnr" : 2, "text" : "doei c2", "conv_id" : 2, "response_to_q" : 1}
+    ]
+
+    convs = [
+        { "conv_id" : 1, "conv_name" : "eerste gesprek"},
+        { "conv_id" : 2, "conv_name" : "tweede gesprek"}
+    ]
--- mockchatbot.py
+++ mockchatbot.py
@@ -1,3 +1,5 @@
+from database.sampledata import Sampledata
+
 # Requirements:
 
 # 1. User provides a message (question), bot responds
@@ -13,15 +15,40 @@
 # 1. Discard determiners (lidwoorden)
 # 2. Performing stemming on given input
 
+sampledata = Sampledata()
+questions = sampledata.getQuestions()
+responses = sampledata.getResponses()
+conversations = sampledata.getConversations()
 
-
-
+print questions
+print responses
+print conversations
 
 def askForInput():
-    try:
-        input = raw_input("Your chat message:\n")
-        print input
-    except Exception, e:
-        print 'error:', e
+    input = raw_input("Your chat message:\n")
+    print input
+    return input
 
-askForInput()
+
+def defineConvId():
+    input = raw_input("\nWhats the conv id?\n")
+    try:
+        conv_id = int(input)
+    except:
+        print 'invalid conv id'
+        exit()
+    return conv_id
+
+
+def getConvName(conv_id):
+    for conv in conversations:
+        if conv_id == conv['conv_id']:
+            print 'the conv name is:', conv['conv_name']
+            return conv['conv_name']
+
+
+
+conv_id = defineConvId()
+conv_name = getConvName(conv_id)
+while True:
+    askForInput()
