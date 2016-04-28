332dacc1c32d26605fb3f48ba7f9484c3b03989c
bastronaut <btsijtsma@gmail.com>
2016-04-28 16:08:30 +0200

    added key to messages and removed r and q

--- database/sampledata.py
+++ database/sampledata.py
@@ -2,12 +2,6 @@
 
 class Sampledata:
 
-    def getQuestions(self):
-        return self.q
-
-    def getResponses(self):
-        return self.r
-
     def getConversations(self):
         return self.convs
 
@@ -17,46 +11,21 @@ class Sampledata:
     def getMessages(self):
         return self.messages
 
-    q = [
-        { "q_nr" : 1, "text" : "hoi1", "conv_id" : 1},
-        { "q_nr" : 2.1, "text" : "hoi21", "conv_id" : 1},
-        { "q_nr" : 2.2, "text" : "hoi22", "conv_id" : 1},
-        { "q_nr" : 2.3, "text" : "hoi23", "conv_id" : 1},
-        { "q_nr" : 3, "text" : "hoi3", "conv_id" : 1},
-        { "q_nr" : 1, "text" : "hoic1", "conv_id" : 2},
-        { "q_nr" : 2, "text" : "hoic2", "conv_id" : 2},
-        { "q_nr" : 1, "text" : "conv3", "conv_id" : 3}
-    ]
-
-    r = [
-        { "r_nr" : 1, "text" : "doei 1", "conv_id" : 1, "response_to_q" : 1},
-        { "r_nr" : 2.1, "text" : "doei 21", "conv_id" : 1, "response_to_q" : 2.1},
-        { "r_nr" : 2.2, "text" : "doei 22", "conv_id" : 1, "response_to_q" : 2.2},
-        { "r_nr" : 2.3, "text" : "doei 23", "conv_id" : 1, "response_to_q" : 2.3},
-        { "r_nr" : 3, "text" : "doei 3", "conv_id" : 1, "response_to_q" : 3},
-        { "r_nr" : 1, "text" : "doei c1", "conv_id" : 2, "response_to_q" : 1},
-        { "r_nr" : 2, "text" : "doei c2", "conv_id" : 2, "response_to_q" : 1},
-        { "r_nr" : 1, "text" : "conv3 response", "conv_id" : 3, "response_to_q" : 1}
-    ]
-
     convs = [
         { "conv_id" : 1, "conv_name" : "first chat"},
         { "conv_id" : 2, "conv_name" : "second chat"},
-        { "conv_id" : 3, "conv_name" : "third chat"}
+        # { "conv_id" : 3, "conv_name" : "third chat"}
     ]
 
-    # TODO
-    # REFACTOR new combined data structure for simplicity in frontend
-
     messages = [
-     { "m_nr" : 1, "qtext" : "hoi1", "rtext" : "doei 1", "is_alternative" : False, "conv_id" : 1 },
-     { "m_nr" : 2, "qtext" : "hoi21", "rtext" : "doei 21", "is_alternative" : True, "conv_id" : 1 },
-     { "m_nr" : 2, "qtext" : "hoi22", "rtext" : "doei 22", "is_alternative" : True, "conv_id" : 1 },
-     { "m_nr" : 2, "qtext" : "hoi23", "rtext" : "doei 23", "is_alternative" : True, "conv_id" : 1 },
-     { "m_nr" : 3, "qtext" : "hoi3", "rtext" : "doei 3", "is_alternative" : False, "conv_id" : 1 },
-     { "m_nr" : 1, "qtext" : "hoic21", "rtext" : "doei c21", "is_alternative" : False, "conv_id" : 2 },
-     { "m_nr" : 2, "qtext" : "hoic22", "rtext" : "doei c22", "is_alternative" : False, "conv_id" : 2 },
-     { "m_nr" : 1, "qtext" : "hoic31", "rtext" : "doei c31", "is_alternative" : False, "conv_id" : 3 },
+     { "m_nr" : 1, "qtext" : "Hi there chatbot", "rtext" : "Hi! :) How are you?", "is_alternative" : False, "conv_id" : 1, "key" : 123 },
+     { "m_nr" : 2, "qtext" : "Good!", "rtext" : "That's great to hear!", "is_alternative" : True, "conv_id" : 1, "key" : 124 },
+     { "m_nr" : 2, "qtext" : "Bad!", "rtext" : "I'm sorry to hear that", "is_alternative" : True, "conv_id" : 1, "key" : 125 },
+     { "m_nr" : 2, "qtext" : "I'm OK!", "rtext" : "nice", "is_alternative" : True, "conv_id" : 1, "key" : 126 },
+     { "m_nr" : 3, "qtext" : "How about you?", "rtext" : "I'm doing just fine!", "is_alternative" : False, "conv_id" : 1, "key" : 127 },
+     { "m_nr" : 1, "qtext" : "Food", "rtext" : "Can I order you anything?", "is_alternative" : False, "conv_id" : 2, "key" : 128 },
+     { "m_nr" : 2, "qtext" : "Pizza", "rtext" : "Ordering you a pizza as we speak!", "is_alternative" : True, "conv_id" : 2, "key" : 129 },
+     { "m_nr" : 2, "qtext" : "Burrito", "rtext" : "Ordering you a burrito as we speak!", "is_alternative" : True, "conv_id" : 2, "key" : 130 },
     ]
 
 
